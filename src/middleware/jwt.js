72f2071bbafa34baa7b499c65e175743af5e8f8b
Devilzer <jenadeepak724@gmail.com>
2021-01-23 04:10:08 +0530

    add jsonwebtoken verification

---
+++ src/middleware/jwt.js
@@ -0,0 +1,28 @@
+require("dotenv").config();
+const jwt = require("jsonwebtoken");
+
+//token verification middleware..
+
+module.exports.authenticate = (req,res,next)=>{
+    try {
+        const authHeader = req.headers["authorization"];
+        const token = authHeader && authHeader.split(" ")[1];
+        if(token==null){
+            return res.status(401).json({
+                message: "Invalid request",
+              });
+        }
+        jwt.verify(token,process.env.ACCESS_TOKEN_SECERT,(err,user)=>{
+            if(err){
+                return res.status(401).json({
+                    message: "Invalid request",
+                  });
+            }
+            next();
+        });
+    } catch (error) {
+        return res.status(400).json({
+            message : `error in authenticating token ${error}`
+        });
+    }
+};
