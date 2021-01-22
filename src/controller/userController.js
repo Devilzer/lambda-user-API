1eb698c2d7f093027283d48416696807429caf80
Devilzer <jenadeepak724@gmail.com>
2021-01-23 04:30:42 +0530

    add user login and access token generation

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -1,6 +1,7 @@
 const User = require("../model/user");
-
-//user sign up meathod
+const jwt = require("jsonwebtoken");
+require('dotenv').config();
+//user sign up method
 
 module.exports.register = async(req,res)=>{
     try {
@@ -28,3 +29,35 @@ module.exports.register = async(req,res)=>{
         });
     }
 };
+
+
+//user sign in method
+
+module.exports.login = async(req,res)=>{
+    try {
+        const user = await User.findOne({phone:req.body.phone,password:req.body.password});
+        if(user){
+            const payload = {
+                name : user.name,
+                phone : user.phone,
+                password :user.password
+            };
+            const accessToken = jwt.sign(payload,process.env.ACCESS_TOKEN_SECERT,{
+                expiresIn : "1d"
+            });
+
+            return res.status(200).json({
+                message : "Login success here is your token.",
+                AccessToken : accessToken
+            });
+        }else{
+            return res.status(401).json({
+                message : "Invalild username or password!"
+            });
+        }
+    } catch (error) {
+        return res.status(400).json({
+            message : `Error in login ${error}`
+        });
+    }
+}
